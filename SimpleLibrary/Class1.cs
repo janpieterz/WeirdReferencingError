2c52448868187aa0d0b181473c72d0a3569252ac
Jan-Pieter Zoutewelle <janpieterzoutewelle@hotmail.com>
2016-05-18 09:16:50 +0200

    Added example

---
+++ SimpleLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLibrary
+{
+    public class SimpleTest
+    {
+        public string Name { get; set; }
+    }
+}
